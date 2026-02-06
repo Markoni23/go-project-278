cf04cfd89d01935400f3e97a60e242f88a8afa9b
Zhigulin Mark <zhigulinmark@gmail.com>
2026-02-06 19:26:41 +0300

    3.0

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func setupEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
+	router.Use(sentrygin.New(sentrygin.Options{}))
+
+	router.GET("/", func(ctx *gin.Context) {
+		if hub := sentrygin.GetHubFromContext(ctx); hub != nil {
+			hub.WithScope(func(scope *sentry.Scope) {
+				scope.SetExtra("unwantedQuery", "someQueryDataMaybe")
+				hub.CaptureMessage("User provided unwanted query string, but we recovered just fine")
+			})
+		}
+		ctx.Status(http.StatusOK)
+	})
+
 	router.GET("/ping", func(c *gin.Context) {
+		//panic("test")
+
 		c.String(http.StatusOK, "pong")
 	})
 
@@ -17,6 +43,13 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	setupEnv()
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_DSN"),
+	}); err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
+
 	router := setupRouter()
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
